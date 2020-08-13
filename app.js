675f9b7ef9b262f65a8668a63875b91fdd901347
Bradford N. Roarr <bradford_roarr@brown.edu>
2020-08-13 10:48:24 -0400

    Adding express js skeleton

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Load up the environment variables.
+require('dotenv').config()
 
-var app = express();
+const indexRouter = require('./routes/index');
+const projectsRouter = require('./routes/projects');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(jwt({ secret: process.env.JWT_SECRET }))
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET ping-pong test response. */
+router.get('/ping', function(req, res, next) {
+  res.json({ message: 'Pong!' });
 });
 
 module.exports = router;
